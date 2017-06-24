d40df4e5fe2b36c9d4f8de3a32e868984f0b77d3
bajmer <marcin.bala@interia.pl>
2017-06-24 23:20:19 +0200

    Transformacja projektu vrp do mavena

---
+++ src/main/java/algorithm/Problem.java
@@ -0,0 +1,75 @@
+package algorithm;
+
+import customers.Customer;
+import customers.CustomerDatabase;
+
+/**
+ * Created by Marcin on 2017-06-17.
+ */
+public class Problem {
+    private static int ID = 0;
+    private int problemID;
+    private int problemTypeID;
+    private Customer depot;
+    private int numberOfCustomers;
+    private int numberOfVehicles;
+    private int vehicleCapacity;
+
+    public Problem(int problemTypeID, int numberOfVehicles, int vehicleCapacity) {
+        ID++;
+        this.problemID = ID;
+        this.problemTypeID = problemTypeID;
+        this.depot = CustomerDatabase.getCustomerList().get(0);
+        this.numberOfCustomers = CustomerDatabase.getCustomerList().size() - 1;
+        this.numberOfVehicles = numberOfVehicles;
+        this.vehicleCapacity = vehicleCapacity;
+    }
+
+    public int getProblemID() {
+        return problemID;
+    }
+
+    public void setProblemID(int problemID) {
+        this.problemID = problemID;
+    }
+
+    public int getProblemTypeID() {
+        return problemTypeID;
+    }
+
+    public void setProblemTypeID(int problemTypeID) {
+        this.problemTypeID = problemTypeID;
+    }
+
+    public Customer getDepot() {
+        return depot;
+    }
+
+    public void setDepot(Customer depot) {
+        this.depot = depot;
+    }
+
+    public int getNumberOfCustomers() {
+        return numberOfCustomers;
+    }
+
+    public void setNumberOfCustomers(int numberOfCustomers) {
+        this.numberOfCustomers = numberOfCustomers;
+    }
+
+    public int getNumberOfVehicles() {
+        return numberOfVehicles;
+    }
+
+    public void setNumberOfVehicles(int numberOfVehicles) {
+        this.numberOfVehicles = numberOfVehicles;
+    }
+
+    public int getVehicleCapacity() {
+        return vehicleCapacity;
+    }
+
+    public void setVehicleCapacity(int vehicleCapacity) {
+        this.vehicleCapacity = vehicleCapacity;
+    }
+}
---
+++ src/main/java/customers/Customer.java
@@ -0,0 +1,114 @@
+package customers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by mbala on 22.05.17.
+ */
+public class Customer {
+
+    private static int customerID = 1;
+    private int id;
+    private String name;
+    private double latitude;
+    private double longitude;
+    private double packageWeight;
+    private double packageCapacity;
+    private String minDeliveryHour;
+    private String maxDeliveryHour;
+
+    private Map<Integer, Double> distances = new HashMap<>();
+
+    public Customer(String name, double latitude, double longitude, double packageWeight, double packageCapacity, String minDeliveryHour, String maxDeliveryHour) {
+        this.id = customerID;
+        customerID++;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.packageWeight = packageWeight;
+        this.packageCapacity = packageCapacity;
+        this.minDeliveryHour = minDeliveryHour;
+        this.maxDeliveryHour = maxDeliveryHour;
+    }
+
+    public static int getCustomerID() {
+        return customerID;
+    }
+
+    public static void setCustomerID(int customerID) {
+        Customer.customerID = customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Map<Integer, Double> getDistances() {
+        return distances;
+    }
+
+    public void setDistances(Map<Integer, Double> distances) {
+        this.distances = distances;
+    }
+
+    public double getPackageWeight() {
+        return packageWeight;
+    }
+
+    public void setPackageWeight(double packageWeight) {
+        this.packageWeight = packageWeight;
+    }
+
+    public double getPackageCapacity() {
+        return packageCapacity;
+    }
+
+    public void setPackageCapacity(double packageCapacity) {
+        this.packageCapacity = packageCapacity;
+    }
+
+    public String getMinDeliveryHour() {
+        return minDeliveryHour;
+    }
+
+    public void setMinDeliveryHour(String minDeliveryHour) {
+        this.minDeliveryHour = minDeliveryHour;
+    }
+
+    public String getMaxDeliveryHour() {
+        return maxDeliveryHour;
+    }
+
+    public void setMaxDeliveryHour(String maxDeliveryHour) {
+        this.maxDeliveryHour = maxDeliveryHour;
+    }
+}
